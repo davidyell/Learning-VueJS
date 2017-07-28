dd4d5e4a609f9f4239ad95a630e0f5173981a764
David Yell <dyell@ukwebmedia.com>
2017-07-28 17:02:29 +0100

    Completed episode nine

---
+++ src/ninth.js
@@ -0,0 +1,23 @@
+Vue.component('dave-message', {
+    props: ['title', 'body'],
+    template: `
+        <article class="message" v-show="isVisible">
+          <div class="message-header">
+            <p>{{ title }}</p>
+            <button class="delete" v-on:click="isVisible = false"></button>
+          </div>
+          <div class="message-body">
+            {{ body }}
+          </div>
+        </article>
+    `,
+    data() {
+        return {
+            isVisible: true
+        };
+    }
+});
+
+new Vue({
+    el: '#example'
+});
