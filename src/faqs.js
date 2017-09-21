fe861c38692734f8ba6212048cd77db46c131b04
David Yell <dyell@ukwebmedia.com>
2017-09-21 10:48:37 +0100

    Swapped more jQuery for VueJS

--- src/faqs.js
+++ src/faqs.js
@@ -25,10 +25,10 @@ Vue.component('faqs', {
         
         <div class="add">
             <div class="form-group text">
-                <input type="text" class="form-control" name="faq-question" id="faq-question">
+                <input type="text" class="form-control" name="faq-question" id="faq-question" v-model="newFaq.question">
             </div>
             <div class="form-group textarea">
-                <textarea name="faq-answer" id="faq-answer" class="form-control"></textarea>
+                <textarea name="faq-answer" id="faq-answer" class="form-control" v-model="newFaq.answer"></textarea>
             </div>
             <input type="submit" class="btn btn-default btn-block" value="Add" v-on:click="create">
         </div>
@@ -41,22 +41,23 @@ Vue.component('faqs', {
                 { id: 9, question: "How many of the things?", answer: "Ooh lots", isUpdate: false },
                 { id: 11, question: "What things begin with traffic?", answer: "Traffic cops and traffic cones", isUpdate: false },
                 { id: 14, question: "Why are Trout the king of fish?", answer: "We'll they're not Koi about it", isUpdate: false }
-            ]
+            ],
+            newFaq: {
+                id: null,
+                question: "",
+                answer: "",
+                isUpdate: false
+            }
         }
     },
     methods: {
         create(event) {
-            console.log(event);
-            let faq = {
-                id: null,
-                question: $("#faq-question").val(),
-                answer: $("#faq-answer").val(),
-                isUpdate: false
-            };
+            // Clone the object instead of passing by reference
+            let faq = JSON.parse(JSON.stringify(this.newFaq));
             this.faqs.push(faq);
 
-            $("#faq-question").val("");
-            $("#faq-answer").val("");
+            this.newFaq.question = "";
+            this.newFaq.answer = "";
         },
         update(event) {
             this.faqs[event.target.dataset.index].isUpdate = true;
