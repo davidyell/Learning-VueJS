dd87991c2cf5b85dce7c1de5180f1512dd18fe25
David Yell <dyell@ukwebmedia.com>
2017-09-21 13:04:47 +0100

    Refactored the FAQ out into it's own component

--- src/faqs.js
+++ src/faqs.js
@@ -1,38 +1,29 @@
 
 Vue.component('faqs', {
     template: `
-    <div id="faqs">
-        <ul class="list-group" id="faqs-list">
-            <li class="list-group-item" v-for="(faq, index) in faqs" v-bind:data-id="faq.id">
-                <div class="read" v-if="!faq.isUpdate">
-                    <span class="glyphicon glyphicon-remove pull-right" v-on:click="remove" v-bind:data-id="faq.id" v-bind:data-index="index"></span>
-                    <span class="glyphicon glyphicon-pencil pull-right" v-on:click="update" v-bind:data-id="faq.id" v-bind:data-index="index"></span>
-                    <h4 class="list-item-heading">{{faq.question}}</h4>
-                    <p>{{faq.answer}}</p>
+        <div id="faqs">
+            <ul class="list-group" id="faqs-list">
+                <faq v-for="(faq, index) in faqs" 
+                    v-bind:key="faq.id" 
+                    v-bind:id="faq.id" 
+                    v-bind:question="faq.question"
+                    v-bind:answer="faq.answer"
+                    v-bind:index="index"
+                    >
+                    {{ faq }}
+                </faq>
+            </ul>
+            
+            <div class="add">
+                <div class="form-group text">
+                    <input type="text" class="form-control" name="faq-question" id="faq-question" v-model="newFaq.question">
                 </div>
-                
-                <div class="update" v-if="faq.isUpdate">
-                    <div class="form-group text">
-                        <input type="text" class="form-control" name="faq-question" id="faq-question" v-model="faq.question">
-                    </div>
-                    <div class="form-group textarea">
-                        <textarea name="faq-answer" id="faq-answer" class="form-control" v-model="faq.answer"></textarea>
-                    </div>
-                    <input type="submit" class="btn btn-default btn-block" value="Update" v-on:click="save" v-bind:data-index="index">
+                <div class="form-group textarea">
+                    <textarea name="faq-answer" id="faq-answer" class="form-control" v-model="newFaq.answer"></textarea>
                 </div>
-            </li>
-        </ul>
-        
-        <div class="add">
-            <div class="form-group text">
-                <input type="text" class="form-control" name="faq-question" id="faq-question" v-model="newFaq.question">
+                <input type="submit" class="btn btn-default btn-block" value="Add" v-on:click="create">
             </div>
-            <div class="form-group textarea">
-                <textarea name="faq-answer" id="faq-answer" class="form-control" v-model="newFaq.answer"></textarea>
-            </div>
-            <input type="submit" class="btn btn-default btn-block" value="Add" v-on:click="create">
         </div>
-    </div>
     `,
     data() {
         return {
@@ -62,16 +53,9 @@ Vue.component('faqs', {
             this.newFaq.question = "";
             this.newFaq.answer = "";
         },
-        update(event) {
-            this.faqs[event.target.dataset.index].isUpdate = true;
-        },
         save(event) {
             // -> Would ajax to remote data store to save
             this.faqs[event.target.dataset.index].isUpdate = false;
-        },
-        remove(event) {
-            // -> Would ajax to remote data store to delete
-            this.faqs.splice(event.target.dataset.index, 1);
         }
     },
     mounted() {
@@ -89,6 +73,53 @@ Vue.component('faqs', {
                 component.sortorder = result;
             }
         });
+    }
+});
+
+Vue.component('faq', {
+    template: `
+        <li class="list-group-item" v-bind:data-id="id">
+            <div class="read" v-if="!isUpdate">
+                <span class="glyphicon glyphicon-remove pull-right" v-on:click="remove" v-bind:data-id="id" v-bind:data-index="index"></span>
+                <span class="glyphicon glyphicon-pencil pull-right" v-on:click="update" v-bind:data-id="id" v-bind:data-index="index"></span>
+                <h4 class="list-item-heading">{{question}}</h4>
+                <p>{{answer}}</p>
+            </div>
+            
+            <div class="update" v-if="isUpdate">
+                <div class="form-group text">
+                    <input type="text" class="form-control" name="faq-question" id="faq-question" v-model="question">
+                </div>
+                <div class="form-group textarea">
+                    <textarea name="faq-answer" id="faq-answer" class="form-control" v-model="answer"></textarea>
+                </div>
+                <input type="submit" class="btn btn-default btn-block" value="Update" v-on:click="save" v-bind:data-index="index">
+            </div>
+        </li>
+    `,
+    props: {
+        id: {
+            required: false
+        },
+        question: {
+            required: true
+        },
+        answer: {
+            required: true
+        },
+        index: {},
+        isUpdate: {
+            default: false
+        }
+    },
+    methods: {
+        remove(event) {
+            // -> Would ajax to remote data store to delete
+            this.faqs.splice(event.target.dataset.index, 1);
+        },
+        update(event) {
+            this.faqs[event.target.dataset.index].isUpdate = true;
+        },
     }
 });
 
